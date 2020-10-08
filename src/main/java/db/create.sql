3d3f56866262cd6df400dc81bac293bbee40e80f
Aliaksandr Zelianko <zely20@mail.ru>
2020-10-08 17:05:46 +0300

    add BD properties

---
+++ src/main/java/db/create.sql
@@ -0,0 +1,3 @@
+    CREATE TABLE rabbit (
+        created date PRIMARY KEY
+    );
--- src/main/java/ru/job4j/quartz/AlertRabbit.java
+++ src/main/java/ru/job4j/quartz/AlertRabbit.java
@@ -3,20 +3,50 @@ package ru.job4j.quartz;
 import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
 
-import java.io.*;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
-import static org.quartz.JobBuilder.*;
-import static org.quartz.TriggerBuilder.*;
-import static org.quartz.SimpleScheduleBuilder.*;
+import static org.quartz.JobBuilder.newJob;
+import static org.quartz.SimpleScheduleBuilder.simpleSchedule;
+import static org.quartz.TriggerBuilder.newTrigger;
 
-    public class AlertRabbit {
-        public static void main(String[] args) {
-            try (InputStream in = AlertRabbit.class.getClassLoader().getResourceAsStream("rabbit.properties")){
-                Properties config = new Properties();
-                config.load(in);
+public class AlertRabbit {
+
+    public static Connection connection(Properties config) {
+        try {
+            Class.forName(config.getProperty("driver-class-name"));
+            return DriverManager.getConnection(
+                    config.getProperty("url"),
+                    config.getProperty("username"),
+                    config.getProperty("password")
+            );
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        try (InputStream in = AlertRabbit.class.getClassLoader().getResourceAsStream("rabbit.properties")) {
+            List<Long> store = new ArrayList<>();
+            Properties config = new Properties();
+            config.load(in);
+            try (Connection connection = connection(config)) {
                 Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
                 scheduler.start();
+                JobDataMap data = new JobDataMap();
+                data.put("store", store);
+                data.put("connection", connection);
                 JobDetail job = newJob(Rabbit.class).build();
                 SimpleScheduleBuilder times = simpleSchedule()
                         .withIntervalInSeconds(Integer.parseInt(config.getProperty("rabbit.interval")))
@@ -26,19 +56,36 @@ import static org.quartz.SimpleScheduleBuilder.*;
                         .withSchedule(times)
                         .build();
                 scheduler.scheduleJob(job, trigger);
-            } catch (SchedulerException se) {
-                se.printStackTrace();
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
+            } catch (SQLException e) {
                 e.printStackTrace();
             }
+        } catch (SchedulerException se) {
+            se.printStackTrace();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+    }
+
 
         public static class Rabbit implements Job {
+
+            public Rabbit() {
+                System.out.println(hashCode());
+
+            }
             @Override
             public void execute(JobExecutionContext context) throws JobExecutionException {
                 System.out.println("Rabbit runs here ...");
+                List<Long> store = (List<Long>) context.getJobDetail().getJobDataMap().get("store");
+                Connection connection = (Connection) context.getJobDetail().getJobDataMap().get("connection");
+                try (PreparedStatement ps = connection.prepareStatement("INSERT INTO rabbit (created) VALUES (?);")) {
+                    ps.setLong(1, System.currentTimeMillis());
+                    ps.execute();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
             }
         }
     }
