8618c1c5b4355b712cc044fcffa0a3d4f8ee2253
zely20@mail.ru <Civik1984>
2020-10-08 20:21:47 +0300

    2.1.1. Парсинг https://www.sql.ru/forum/job-offers/3[285210#329094]

--- src/main/java/ru/job4j/quartz/SqlRuParser.java
+++ src/main/java/ru/job4j/quartz/SqlRuParser.java
@@ -7,19 +7,26 @@ import org.jsoup.select.Elements;
 
 public class SqlRuParser {
     public static void main(String[] args) throws Exception {
-        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
-        Elements row = doc.select(".postslisttopic");
-        for (Element td : row) {
-            Element href = td.child(0);
-            System.out.println(href.attr("href"));
-            System.out.println(href.text());
+        Document doc;
+        for (int i = 0; i < 6; i++) {
+            if (i == 0) {
+                doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
+            } else {
+                doc = Jsoup.connect("https://www.sql.ru/forum/job-offers/" + i).get();
+            }
+            Elements row = doc.select(".postslisttopic");
+            for (Element td : row) {
+                Element href = td.child(0);
+                System.out.println(href.attr("href"));
+                System.out.println(href.text());
+            }
         }
-        Elements d = doc.select(".altCol");
+/*        Elements d = doc.select(".altCol");
         for (Element td : d) {
             Elements innerElements = td.children();
             if (innerElements.isEmpty()){
                 System.out.println(td.text());
             }
-        }
+        }*/
     }
 }
